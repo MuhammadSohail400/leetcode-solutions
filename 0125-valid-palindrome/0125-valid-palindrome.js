0d426d42f18b73fafe4832ee029dcc7fe1930061
Muhammad Sohail <msohailg211@gmail.com>
2026-08-30 10:04:22 +0500

    Time: 6 ms (50.89%), Space: 58.1 MB (50.46%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+
+    let clean = s
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, "");
+
+    let left = 0;
+    let right = clean.length - 1;
+
+    while (left < right) {
+
+        if (clean[left] !== clean[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+};
